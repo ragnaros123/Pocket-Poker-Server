b8a71f07749215d1f89040198e30cfa4caabd9a1
ragnaros123 <68516384+ragnaros123@users.noreply.github.com>
2022-12-01 20:06:54 +0000

    hi

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ dbo.connectToServer(function (err) {
   }
 
   // start the Express server
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port: ${PORT}`);
   });
 });
